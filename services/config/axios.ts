620679d4ba7168a8a3767aebfa6c2ba8c43deaf1
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-08-23 00:24:23 +0900

    Set: axios 설정 추가

--- services/config/axios.ts
+++ services/config/axios.ts
@@ -1,8 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
 // 인스턴스를 생성할때 config 기본값 설정하기
 const instance = axios.create({
-  baseURL: "https://api.example.com"
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  timeout: 30000
 });
+
+// response interceptor
+instance.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    console.log(error);
+  }
+);
 
 export default instance;
