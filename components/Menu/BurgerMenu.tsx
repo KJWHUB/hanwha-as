897e79e6ca02f4a703eaa3ae5a7842fc75fcf80a
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-09-16 00:19:28 +0900

    Feature: 햄버거 메뉴 구현

--- components/Menu/BurgerMenu.tsx
+++ components/Menu/BurgerMenu.tsx
@@ -1,8 +1,20 @@
-import '/styles/hamburgers.css';
+'use client';
+
+import { useState } from 'react';
 
 export default function BurgerMenu() {
+  const [hamburgerIsActive, setHamburgerIsActive] = useState(false);
+
+  const chageHamburgerState = () => {
+    setHamburgerIsActive(!hamburgerIsActive);
+  };
+
   return (
-    <button className="hamburger" type="button">
+    <button
+      className={`hamburger hamburger--squeeze ${hamburgerIsActive ? 'is-active' : ''}`}
+      type="button"
+      onClick={chageHamburgerState}
+    >
       <span className="hamburger-box">
         <span className="hamburger-inner"></span>
       </span>
