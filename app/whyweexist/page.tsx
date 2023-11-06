97410d3ca8050e64af97b5547b80d42c45d0c600
kjw1025_mac_studio <126055193+KJWHUB@users.noreply.github.com>
2023-11-06 15:09:37 +0900

    Set: firestore 제거

--- app/whyweexist/page.tsx
+++ app/whyweexist/page.tsx
@@ -1,29 +1,9 @@
 'use client';
 
-import { useState } from 'react';
-import { collection, addDoc } from 'firebase/firestore';
-import db from '@/firebaseMySet';
-
 export default function Home() {
-  const [value, setValue] = useState('');
-
-  const onClickUpLoadButton = async () => {
-    //    addDoc(collection(db       , "컬렉션이름") , { 추가할 데이터 }
-    await addDoc(collection(db, `testData`), {
-      value
-    });
-  };
-
   return (
     <main>
       <p>asdsd</p>
-
-      <form onSubmit={(event) => event.preventDefault()}>
-        <input onChange={(event) => setValue(event.target.value)} />
-        <button type="button" onClick={onClickUpLoadButton}>
-          전송
-        </button>
-      </form>
     </main>
   );
 }
